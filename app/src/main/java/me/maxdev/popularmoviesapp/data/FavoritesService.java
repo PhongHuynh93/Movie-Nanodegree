7a3c6158393b8032b1b88cc784183fd771d6118e
Phong <p7157568@gmail.com>
2016-07-28 08:19:04 +0700

    Initial commit

---
+++ app/src/main/java/me/maxdev/popularmoviesapp/data/FavoritesService.java
@@ -0,0 +1,60 @@
+package me.maxdev.popularmoviesapp.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+
+// TODO: 7/28/16 this class will add or remove to db for you
+// TODO: 7/28/16 change to use db from 3 source: server + db + cache
+public class FavoritesService {
+
+    private static volatile FavoritesService instance = null;
+
+    private final Context context;
+
+    public FavoritesService(Context context) {
+        if (instance != null) {
+            throw new IllegalStateException("Already instantiated.");
+        }
+        this.context = context.getApplicationContext();
+    }
+
+    public static FavoritesService getInstance(Context context) {
+        synchronized (FavoritesService.class) {
+            if (instance == null) {
+                instance = new FavoritesService(context);
+            }
+        }
+        return instance;
+    }
+
+    public void addToFavorites(Movie movie) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MoviesContract.COLUMN_MOVIE_ID_KEY, movie.getId());
+        context.getContentResolver().insert(MoviesContract.Favorites.CONTENT_URI, contentValues);
+    }
+
+    public void removeFromFavorites(Movie movie) {
+        context.getContentResolver().delete(
+                MoviesContract.Favorites.CONTENT_URI,
+                MoviesContract.COLUMN_MOVIE_ID_KEY + " = " + movie.getId(),
+                null
+        );
+    }
+
+    public boolean isFavorite(Movie movie) {
+        boolean favorite = false;
+        Cursor cursor = context.getContentResolver().query(
+                MoviesContract.Favorites.CONTENT_URI,
+                null,
+                MoviesContract.COLUMN_MOVIE_ID_KEY + " = " + movie.getId(),
+                null,
+                null
+        );
+        if (cursor != null) {
+            favorite = cursor.getCount() != 0;
+            cursor.close();
+        }
+        return favorite;
+    }
+}
---
+++ app/src/main/java/me/maxdev/popularmoviesapp/ui/grid/MoviesGridFragment.java
@@ -0,0 +1,140 @@
+package me.maxdev.popularmoviesapp.ui.grid;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import me.maxdev.popularmoviesapp.R;
+import me.maxdev.popularmoviesapp.data.MoviesService;
+import me.maxdev.popularmoviesapp.data.SortHelper;
+import me.maxdev.popularmoviesapp.ui.EndlessRecyclerViewOnScrollListener;
+import me.maxdev.popularmoviesapp.ui.SortingDialogFragment;
+
+// TODO: 7/28/16 this is a view for showing a list of popular movies
+public class MoviesGridFragment extends AbstractMoviesGridFragment {
+
+    private MoviesService moviesService;
+    private SortHelper sortHelper;
+    private EndlessRecyclerViewOnScrollListener endlessRecyclerViewOnScrollListener;
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(MoviesService.BROADCAST_UPDATE_FINISHED)) {
+                if (!intent.getBooleanExtra(MoviesService.EXTRA_IS_SUCCESSFUL_UPDATED, true)) {
+                    Snackbar.make(swipeRefreshLayout, R.string.error_failed_to_update_movies,
+                            Snackbar.LENGTH_LONG)
+                            .show();
+                }
+                swipeRefreshLayout.setRefreshing(false);
+                endlessRecyclerViewOnScrollListener.setLoading(false);
+                updateGridLayout();
+            } else if (action.equals(SortingDialogFragment.BROADCAST_SORT_PREFERENCE_CHANGED)) {
+                recyclerView.smoothScrollToPosition(0);
+                restartLoader();
+            }
+        }
+    };
+
+    public static MoviesGridFragment create() {
+        return new MoviesGridFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        sortHelper = new SortHelper(PreferenceManager.getDefaultSharedPreferences(getContext()));
+        moviesService = MoviesService.getInstance(getContext());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(MoviesService.BROADCAST_UPDATE_FINISHED);
+        intentFilter.addAction(SortingDialogFragment.BROADCAST_SORT_PREFERENCE_CHANGED);
+        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(broadcastReceiver, intentFilter);
+        if (endlessRecyclerViewOnScrollListener != null) {
+            endlessRecyclerViewOnScrollListener.setLoading(moviesService.isLoading());
+        }
+        swipeRefreshLayout.setRefreshing(moviesService.isLoading());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(broadcastReceiver);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_movies_grid, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_show_sort_by_dialog:
+                showSortByDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    @NonNull
+    protected Uri getContentUri() {
+        return sortHelper.getSortedMoviesUri();
+    }
+
+    @Override
+    protected void onCursorLoaded(Cursor data) {
+        getAdapter().changeCursor(data);
+        if (data == null || data.getCount() == 0) {
+            refreshMovies();
+        }
+    }
+
+    @Override
+    protected void onRefreshAction() {
+        refreshMovies();
+    }
+
+    @Override
+    protected void onMoviesGridInitialisationFinished() {
+        endlessRecyclerViewOnScrollListener = new EndlessRecyclerViewOnScrollListener(getGridLayoutManager()) {
+            @Override
+            public void onLoadMore() {
+                swipeRefreshLayout.setRefreshing(true);
+                moviesService.loadMoreMovies();
+            }
+        };
+        recyclerView.addOnScrollListener(endlessRecyclerViewOnScrollListener);
+    }
+
+    private void refreshMovies() {
+        swipeRefreshLayout.setRefreshing(true);
+        moviesService.refreshMovies();
+    }
+
+    private void showSortByDialog() {
+        DialogFragment sortingDialogFragment = new SortingDialogFragment();
+        sortingDialogFragment.show(getFragmentManager(), SortingDialogFragment.TAG);
+    }
+}
